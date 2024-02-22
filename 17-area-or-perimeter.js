966033b06c01c839da4a977cf656b7ed7f2a825c
AffanSurya <affanqwe04@gmail.com>
2024-02-22 12:25:14 +0700

    update

--- 17-area-or-perimeter.js
+++ 17-area-or-perimeter.js
@@ -1,6 +1,6 @@
 // 17/366
 // https://www.codewars.com/kata/5ab6538b379d20ad880000ab
 
-const areaOrPerimeter = (l, w) => (l === w ? l * w : l + l + w + w);
+const areaOrPerimeter = (l, w) => (l === w ? l * w : 2 * (l + w));
 
 console.log(areaOrPerimeter(6, 10));
