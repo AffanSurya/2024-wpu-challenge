cb2aab821ec6eae5b9c1c79bf39cbe9e02e00fe3
AffanSurya <affanqwe04@gmail.com>
2024-02-25 21:42:24 +0700

    day 18

---
+++ 18-grasshopper.js
@@ -0,0 +1,8 @@
+// 18/366
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7
+
+function greet(name, owner) {
+    return name === owner ? "Hello boss" : "Hello guest";
+}
+
+console.log(greet("Daniel", "Daniel"));
